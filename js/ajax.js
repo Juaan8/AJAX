0c2a4e3ba0d0a496800bccc00448da07b006538b
Juan <juancogo38@gmail.com>
2021-08-22 17:48:21 -0500

    API Fetch

--- js/ajax.js
+++ js/ajax.js
@@ -38,3 +38,47 @@
 
 	xhr.send();
 })();
+
+
+
+/*--------API Fetch--------*/
+
+(() => {
+	const $fetch = document.getElementById("fetch"),
+		$fragment = document.createDocumentFragment();
+
+	fetch("https://jsonplaceholder.typicode.com/users")
+
+	/*.then(res => {
+		console.log(res);
+		return res.ok ? res.text() : Promise.reject(res);
+	})*/
+
+	.then((res) => (res.ok ? res.json() : Promise.reject(res)))
+
+	.then((json) =>{
+		console.log(json);
+		//$fetch.innerHTML = json;
+
+		json.forEach((el) => {
+			const $li = document.createElement("li");
+			$li.innerHTML = `${el.name}--${el.email}--${el.phone}`;
+			$fragment.appendChild($li);
+			});
+
+		$fetch.appendChild($fragment);
+	})
+
+	.catch((err) => {
+		console.log(err);
+		let message = err.statusText || "ocurrio un error";
+			$fetch.innerHTML = `Error ${err.status}: ${message}`;
+	})
+
+	.finally(() => 
+	console.log(
+		"Este mensaje se cargara sea cual sea el resultado de la promesa fetch"
+		)
+	);
+
+})();
